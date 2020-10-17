59aab54bfaafef677883ea1968d2a35bae35b99d
MNicolso <71354582+MNicolso@users.noreply.github.com>
2020-10-17 09:34:54 -0700

    Table function

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -32,3 +32,67 @@ function homeLoan(){
     document.getElementById("interestOut").innerHTML = `${interestRound}`;
     document.getElementById("costOut").innerHTML = `${totalRound}`;
 }
+
+document.getElementById("homeLoanButton").addEventListener("click", table)
+
+
+function table() {
+    var amount = document.getElementById("amount").value;
+    var rate = document.getElementById("rate").value;
+    var months = document.getElementById("months").value;
+
+    var monthsCollected = [];
+    monthsCollected[months] = "";
+    currentMonth = 0;
+
+    for (i = 0; i <= months; i++) {
+        currentMonth++;
+
+        if (monthsCollected[i] == undefined) {
+            monthsCollected[i] = currentMonth;
+        }
+
+    }
+    monthsCollected.pop();
+
+    //table arrays
+    var acountBalance = [amount];
+    var monthlyPaymentsCollected = [monthly];
+    var InterestBalance = [];
+    var totalPayments = [];
+    var principalBalance = [];
+
+    for (i = 0; i < months; i++) {
+        var monthsTableOutput;
+
+
+        //interest Payment  
+        var iBalance = acountBalance[i] * (rate / 1200);
+        InterestBalance.push(iBalance);
+
+        //sum interest
+        var sum = InterestBalance.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+
+        //Total Payment
+
+        var a = rate / 1200;
+        var b = (1 + a) ** -months;
+        var monthly = amount * a / (1 - b);
+
+        totalPayments.push(monthly);
+
+        //Principle
+        var pBalance = monthly - InterestBalance[InterestBalance.length - 1];
+        principalBalance.push(pBalance);
+
+        //balance
+        var balance = acountBalance[i] - principalBalance[i];
+        acountBalance.push(balance);
+
+        monthsTableOutput += "<tr>" + "<td>" + monthsCollected[i] + "</td>" + "<td>" + monthly.toFixed(2) + "<td>" + principalBalance[principalBalance.length - 1].toFixed(2) + "</td>" + "</td>" + "<td>" + InterestBalance[InterestBalance.length - 1].toFixed(2) + "</td>" + "<td>" + sum.toFixed(2) + "</td>" + "<td>" + acountBalance[acountBalance.length - 1].toFixed(2) + "</td>" + "</tr>";
+
+        document.getElementById('monthsOutput').innerHTML = monthsTableOutput;
+    }
+}
